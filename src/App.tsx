8315539c6d4b1b6129406dfa59efe5966a42566a
Levis155 <levismbui@gmail.com>
2025-08-21 06:19:40 +0300

    place all required elements on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <div>Hello world!</div>
-  )
+    <div className="app-wrapper">
+      <div className="calculator">
+        <div className="display">
+          <span className="expression"></span>
+          <span className="result"></span>
+        </div>
+
+        <div className="buttons-grid">
+          <button className="clear span-2">AC</button>
+          <button className="operator">/</button>
+          <button className="operator">x</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="operator">-</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button className="operator">+</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button className="equal">=</button>
+          <button className="zero">0</button>
+          <button>.</button>
+        </div>
+      </div>
+
+      <span>Designed and coded by <br /> <a href="https://www.linkedin.com/in/levis-mbui/" target="_blank">Levis Mbui</a></span>
+    </div>
+  );
 }
 
-export default App
+export default App;
